66af5358021d11ce2c5ef8f0955c51dfb86b5ddb
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2026-03-05 12:05:00 +0800

    dev: update libraries, husky, and add cleanup

--- hydra-app/src/__tests__/App.test.tsx
+++ hydra-app/src/__tests__/App.test.tsx
@@ -1,18 +1,15 @@
 import { describe, test, expect, beforeEach } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { render, screen, cleanup } from '@testing-library/react'
 
 import { App } from '@pages/App'
 
 describe('App component', () => {
   beforeEach(() => {
+    cleanup()
     render(<App />)
   })
 
   test('It should work w/o errors', () => {
     expect(screen.findByTestId('hydra-app')).toBeTruthy()
-  })
-
-  test('It should display "Hello World!"', async () => {
-    expect(screen.getByTestId('banner-heading').textContent).toBe('Hello World!')
   })
 })
