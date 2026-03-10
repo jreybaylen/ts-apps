6b19236ed0bbfbd03ea0fc3d2e056b8b42441ca4
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2026-03-11 01:59:11 +0800

    dev: redis with bun sql

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -1,5 +1,33 @@
+import { SQL } from 'bun'
 import { Elysia } from 'elysia'
+import Redis from 'ioredis'
 
-const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
+const redis = new Redis()
+const psql = new SQL({
+  host: 'localhost',
+  port: 5432,
+  password: 'freelance2024',
+  username: 'jreybaylen',
+  database: 'postgres'
+})
+
+async function redisTest() {
+  const redisData = await redis.get('data')
+
+  if (redisData) {
+    const dataFromRedis = JSON.parse(redisData)
+    console.log('Data from redis...', dataFromRedis)
+    return dataFromRedis
+  }
+
+  const dbTestRedis = await psql`SELECT * FROM test_redis`
+  console.log('Data from database...', dbTestRedis)
+
+  await redis.setex('data', 60, JSON.stringify(dbTestRedis))
+
+  return dbTestRedis
+}
+
+const app = new Elysia().get('/', redisTest).listen(3000)
 
 console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`)
