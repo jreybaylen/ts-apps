dc861bb6b24eb3f5be23eaccdb8db1d9899da1bd
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2026-03-11 13:42:18 +0800

    dev: update husky, lint-staged, and scripts

--- hydra-app/service/src/index.ts
+++ hydra-app/service/src/index.ts
@@ -1,7 +1,5 @@
-import { Elysia } from "elysia";
+import { Elysia } from 'elysia'
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia().get('/', () => 'Hello Elysia').listen(3000)
 
-console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
-);
+console.log(`🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port} `)
