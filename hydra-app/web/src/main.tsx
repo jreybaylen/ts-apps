c556f02317a18ba246d7076f2eb476d5dc9ffef3
John Rey Baylen <john.rey.baylen.pro@gmail.com>
2026-03-11 12:45:18 +0800

    dev: apply baseUrl and paths

--- hydra-app/web/src/main.tsx
+++ hydra-app/web/src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
-import { App } from './pages/App'
+import { App } from '@pages/App'
 
 const htmlRootElement = document.getElementById('root') as HTMLElement
 const rootElement = createRoot(htmlRootElement)
